4d164f23802cbe147f69ef96b572ab1ec9b249d0
zj82 <115696232+Condor2436@users.noreply.github.com>
2023-02-14 00:50:12 +0800

    upload demo

--- src/main/java/edu/duke/zj82/battleship/AbstractShipFactory.java
+++
@@ -1,9 +0,0 @@
-package edu.duke.zj82.battleship;
-
-public interface AbstractShipFactory<T> {
-    // functions to make different types of ships
-    public Ship<T> makeSubmarine(Placement where);
-    public Ship<T> makeBattleship(Placement where);
-    public Ship<T> makeCarrier(Placement where);
-    public Ship<T> makeDestroyer(Placement where);
-}
--- src/main/java/edu/duke/zj82/battleship/ShipDisplayInfo.java
+++
@@ -1,6 +0,0 @@
-package edu.duke.zj82.battleship;
-
-public interface ShipDisplayInfo<T> {
-    // get corresponding display information
-    public T getInfo(Coordinate where, boolean hit);
-}
